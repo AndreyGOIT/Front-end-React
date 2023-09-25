665f3717ff57d938e1631cf0f806fa9d4555dcc3
AndreyGOIT <ae.borga@gmail.com>
2023-09-25 09:43:26 +0300

    form-page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import TodoForm from "./component/TodoForm";
+// import TodoForm from "./component/TodoForm";
+import FormPage from "./component/AddButton";
 
 function App() {
   return (
     <div className="App">
-      <TodoForm />
+      <FormPage />
+      {/* <TodoForm /> */}
     </div>
   );
 }
